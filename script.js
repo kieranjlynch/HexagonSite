83e04d2db1b42031abd24876a4c165ea42108792
Kieran Lynch <49360558+kieranjlynch@users.noreply.github.com>
2025-01-01 16:40:32 +0000

    updated site content to latest info

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const downloadBtn = document.querySelector('.download-btn');
-    downloadBtn.addEventListener("click", () => {
-        alert("Redirecting to the App Store...");
-    });
-});
